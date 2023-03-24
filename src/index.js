17307799766b8cbdd77c5385a494ba751f55bce5
ammar <ammar.ali.haidar.1990@gmail.com>
2023-03-24 09:40:06 +0300

    done-fixed

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { AuthContextProvider } from './AuthContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='dashboard-auth'>
     <ModeContextProvider>
       <AuthContextProvider >
     <App />
